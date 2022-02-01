97ce29c5433f210c869c3edfce1bf666fe07f30d
Oliver Francis <olliefrancis97@hotmail.co.uk>
2022-02-01 11:26:25 +0000

    frontend ux/ui modifications

--- webpack.config.dapp.js
+++ webpack.config.dapp.js
@@ -1,5 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+//const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = {
   entry: ['@babel/polyfill', path.join(__dirname, "src/dapp")],
@@ -31,21 +33,44 @@ module.exports = {
       }
     ]
   },
+
+  // optimization: {
+  //   minimizer: [
+  //     // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+  //       `...`,
+  //     new CssMinimizerPlugin(),
+  //   ],
+  // },
+
+
   plugins: [
     new HtmlWebpackPlugin({ 
       template: path.join(__dirname, "src/dapp/index.html")
-    })
+    }),
   ],
+    // new MiniCssExtractPlugin({
+    //   filename: '[name].css',
+    //   chunkFilename: '[id].css',
+    //   ignoreOrder: false,
+    // }),
+
   resolve: {
     extensions: [".js"],
     fallback: {
-        stream: false,
-        crypto: false,
-        assert: false,
-        https: false,
-        http: false,
-        url: false,
-        os: false
+        stream: require.resolve("stream-browserify"),
+        crypto: require.resolve("crypto-browserify"),
+        assert: require.resolve("assert/"),
+        https: require.resolve("https-browserify"),
+        http: require.resolve("stream-http"),
+        url: require.resolve("url/"),
+        os: require.resolve("os-browserify/browser")
+        // stream: false,
+        // crypto: false,
+        // assert: false,
+        // https: false,
+        // http: false,
+        // url: false,
+        // os: false
     }
   },
 
